302b41c535f5d8fe56f83ab49b4e1b6344b1fa8e
HP657 <junlee5070@gmail.com>
2026-09-19 23:29:49 +0900

    feat: init project

---
+++ src/main/kotlin/com/workly/hp657/Hp657Application.kt
@@ -0,0 +1,11 @@
+package com.workly.hp657
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Hp657Application
+
+fun main(args: Array<String>) {
+	runApplication<Hp657Application>(*args)
+}
